15926483c0f69f4a38490e27e33622d4d12804dd
Scott Brady <sbradymobile@gmail.com>
2011-05-29 21:22:13 -0700

    inc: Match light sensor levels to board-incrediblec.c
    Matches light levels from cm-kernel/arch/arm/mach-msm/board-incrediblec.c
    to libsensors/LightSensor.cpp and overlay/frameworks/base/core/res/res/values/config.xml.
    
    Let me know if the brightness values need to change.
    
    Change-Id: Ie8f424e0011da51e30c1b3fd462a29e59c1cbeaf

--- libsensors/LightSensor.cpp
+++ libsensors/LightSensor.cpp
@@ -143,8 +143,8 @@ int LightSensor::readEvents(sensors_event_t* data, int count)
 float LightSensor::indexToValue(size_t index) const
 {
     static const float luxValues[10] = {
-            10.0, 160.0, 225.0, 320.0, 640.0,
-            1280.0, 2600.0, 5800.0, 8000.0, 10240.0
+            1.0, 11.0, 16.0, 22.0, 75.0,
+            209.0, 362.0, 488.0, 560.0, 1023.0
     };
 
     const size_t maxIndex = sizeof(luxValues)/sizeof(*luxValues) - 1;
